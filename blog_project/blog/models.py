bb5f3781e01226db205c5f3107113786de91b84a
AryanShrestha123 <aryan4845i@gmail.com>
2025-06-16 21:59:52 +0545

    Comment model added

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -12,3 +12,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_date',)
+
+    def __str__(self):
+        return f'{self.name} - {self.post.title}'
